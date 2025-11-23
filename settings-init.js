f9e5c6d375f203dd49949f78523f6d4d8a4a2d70
nwhbd <netlinkwebhost@gmail.com>
2025-11-23 09:39:04 +0600

    Add files via upload

---
+++ settings-init.js
@@ -0,0 +1,12 @@
+"use strict"
+var optionSettings = {
+    layout:"wide",
+    background:"light",
+    color:"blue",
+    header:"static",
+    font:"nunito",
+    textDirection:"ltr",
+    radius:"threeradius",
+    showSettings:"hide",
+};
+new antlerSettings(optionSettings);
