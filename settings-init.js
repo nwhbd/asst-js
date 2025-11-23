ccc3174e6cfe0d4668aa997c03697477633552a1
nwhbd <netlinkwebhost@gmail.com>
2025-11-23 09:40:35 +0600

    Update settings-init.js

--- settings-init.js
+++ settings-init.js
@@ -1,12 +1,2 @@
 "use strict"
-var optionSettings = {
-    layout:"wide",
-    background:"light",
-    color:"blue",
-    header:"static",
-    font:"nunito",
-    textDirection:"ltr",
-    radius:"threeradius",
-    showSettings:"hide",
-};
-new antlerSettings(optionSettings);
+var optionSettings={layout:"wide",background:"light",color:"blue",header:"static",font:"nunito",textDirection:"ltr",radius:"threeradius",showSettings:"hide",};new antlerSettings(optionSettings)
